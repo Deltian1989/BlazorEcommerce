d3d355fd98a7ee979c0059c6965a12ebceec54f4
Dorian Jarosz <deltian1989@gmail.com>
2023-11-09 15:49:44 +0100

    Created category controller with its service and created GetProductsByCategory action for the controller (also added category list to the sidebar in the razor page)

---
+++ BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorEcommerce.Server.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly BEContext _dbContext;
+
+        public CategoryService(BEContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategories()
+        {
+            var categories = await _dbContext.Categories.ToListAsync();
+
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories
+            };
+        }
+    }
+}
